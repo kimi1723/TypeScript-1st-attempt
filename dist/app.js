54541467a279a33253efe70d13a1336cd8494bd1
kimi1723 <100174940+kimi1723@users.noreply.github.com>
2023-03-17 14:17:11 +0100

    initalize

---
+++ dist/app.js
@@ -0,0 +1,4 @@
+let age = 19;
+age = 20;
+const add = (v1, v2) => v1 + v2;
+console.log(add(10, 15));
---
+++ src/app.ts
@@ -0,0 +1,7 @@
+let age: number = 19;
+
+age = 20;
+
+const add = (v1: number, v2: number) => v1 + v2;
+
+console.log(add(10, 15));
